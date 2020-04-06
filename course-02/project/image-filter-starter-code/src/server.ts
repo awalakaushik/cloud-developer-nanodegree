484859ab5e34bd7045772704880293dfefd125ca
KAUSHIK REDDY AWALA <kawala@uh.edu>
2020-04-05 19:31:14 -0500

    Add image filter for the Udagram Application (#11)
    * add endpoint for returning filteredImage path
    
    * add AWS EBS deployment dashboard screenshot
    
    * Add description and instructions
    
    Co-authored-by: awalakaushik <awalakaushikreddy@gmail.com>

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -12,25 +12,32 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
   
+  // GET /filteredimage?image_url={{URL}}
+  // Filters the image and returns the file in the response
+  app.get("/filteredimage", async (req, res) => {
+    const {image_url} = req.query;
+    
+    if (!image_url) {
+      res.status(400).send("Invalid Request! Query parameter image_url is missing.");
+    }
+
+    try {
+      const filteredImagePath = await filterImageFromURL(image_url);
+      res.sendFile(filteredImagePath, async (error) => {
+        await deleteLocalFiles([filteredImagePath]);
+        if (res.headersSent && !error) {
+          res.status(200);
+        }
+        res.status(500).send(" Error encountered while returning filteredPath of the image.");
+      });
+    }
+    catch(error) {
+      res.status(500).send("An error occurred while filtering the image!");
+    }
+
+  });
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
